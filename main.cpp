b02f894a8c3d6c9e75b1c9089517be39a018d122
krishnav22 <147203302+krishnav22@users.noreply.github.com>
2024-05-05 22:39:19 +0530

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+class Solution{
+public:
+    //Function to find the days of buying and selling stock for max profit.
+    vector<vector<int> > stockBuySell(vector<int> A, int n){
+        vector<vector<int>> ans;
+        for(int i = 1; i < n; i++){
+            if(A[i] > A[i-1]){
+                ans.push_back({i-1, i});
+            }
+        }
+        return ans;
+    }
+};
